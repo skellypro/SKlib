e3f42723c6e09e6bfc7b6ee39d959502c90b59cf
Sean Kelly <122487768+skellypro@users.noreply.github.com>
2025-11-26 02:03:50 +0000

    fixes in cstring.cpp

--- libc/cstring/cstring.cpp
+++ libc/cstring/cstring.cpp
@@ -13,33 +13,32 @@ extern "C" {
 
 	size_t strlen(const char* str)
 	{
-		register size_t len = 0;
+		size_t len = 0;
 		while (str[len])
 			len++;
 		return len + 1;
 	}
 
 	int strcmp(const char* a, const char* b) {
-		if (a != b) {
-			register int i = 0;
-			do {
-				if (a[i] != b[i])
-					return false;
-				i++;
-			} while (a[i] & b[i]);
-		}
-		return true;
+        int result;
+		if (a == b)
+            result = 0;
+        else
+            for(size_t i = 0; a[i] && b[i]; i++)
+                if(0 != (result = a[i] - b[i]))
+                    break;
+		return result;
 	}
 
 	char* strcat(char* destination, const char* source) {
-		register int i = strlen(destination), d = 0;
-		/*
+		int i = strlen(destination), d = 0;
 		do
 			destination[i++] = source[d++];
 		while(source[d]);
-		*/
+        /*
 		for (destination[i] : c)
 			i++;
+        */
 		return destination;
 	}
 
@@ -52,7 +51,7 @@ extern "C" {
         size_t i = 0;
         while (str[i] && (str[i] != character))
             i++;
-        return ((void*)(&str[i]));
+        return &str[i];
     }
 
 }
