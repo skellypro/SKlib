c7b0ca0933e4bc6accab30068bcc192718839e5c
Sean Kelly <122487768+skellypro@users.noreply.github.com>
2025-11-26 00:16:21 +0000

    skbool.h changed to stdbool.h

--- include/stdbool.h
+++ include/stdbool.h
@@ -1,16 +1,14 @@
 /*
- * skbool.h
+ * stdbool.h
  *
  *  Created on: Feb 27, 2023
  *      Author: sean
+ * standardized on: Nov 25, 2025
  */
 
-#ifndef INCLUDE_SKBOOL_H_
-#define INCLUDE_SKBOOL_H_
+#pragma once
 
-#define bool unsigned char
+#define bool short
 #define true 1
 #define false 0
 #define __bool_true_false_are_defined 1
-
-#endif /* INCLUDE_SKBOOL_H_ */
