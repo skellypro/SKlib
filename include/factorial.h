7545fdf7fca88271b6b37c61844154126cb5d16a
Sean Kelly <122487768+skellypro@users.noreply.github.com>
2026-01-13 22:44:08 +0000

    Basic factorials

---
+++ include/factorial.h
@@ -0,0 +1,15 @@
+#pragma once
+
+extern "C" {
+    short factorial(short n);
+    unsigned short factorial(unsigned short n);
+    int factorial(int n);
+    unsigned factorial(unsigned n);
+    long factorial(long n);
+    unsigned long factorial(unsigned long n);
+    long long factorial(long long n);
+    unsigned long long factorial(unsigned long long n);
+    float factorial(float n);
+    double factorial(double n);
+    long double factorial(long double n);
+}
