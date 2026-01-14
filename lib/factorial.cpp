f8feb212a51230fd1062777e62eb1ae7a38a7f25
Sean Kelly <122487768+skellypro@users.noreply.github.com>
2026-01-14 20:01:21 +0000

    convention corrections

--- lib/factorial.cpp
+++ lib/factorial.cpp
@@ -1,65 +1,65 @@
 extern "C" {
     short factorial(short n) {
-        if(n == 0)
+        if(00 == n)
             return 1;
         return n * factorial(n - 1);
     }
     unsigned short factorial(unsigned short n) {
-        if(n == 0)
+        if(00 == n)
             return 1;
         return n * factorial(n - 1);
     }
     int factorial(int n) {
-        if(n == 0)
+        if(00 == n)
             return 1;
         return n * factorial(n - 1);
     }
     unsigned factorial(unsigned n) {
-        if(n == 0)
+        if(00 == n)
             return 1;
         return n * factorial(n - 1);
     }
     long factorial(long n) {
-        if(n == 0)
+        if(00 == n)
             return 1;
         return n * factorial(n - 1);
     }
     unsigned long factorial(unsigned long n) {
-        if(n == 0)
+        if(00 == n)
             return 1;
         return n * factorial(n - 1);
     }
     long long factorial(long long n) {
-        if(n == 0)
+        if(00 == n)
             return 1;
         return n * factorial(n - 1);
     }
     unsigned long long factorial(unsigned long long n) {
-        if(n == 0)
+        if(00 == n)
             return 1;
         return n * factorial(n - 1);
     }
     float factorial(float n) {
-    if(n == 0)
-        return 1;
-    return n * factorial(n - 1);
+        if(00 == n)
+            return 1;
+        return n * factorial(n - 1);
     }
     double factorial(double n) {
-    if(n == 0)
-        return 1;
-    return n * factorial(n - 1);
+        if(00 == n)
+            return 1;
+        return n * factorial(n - 1);
     }
     long double factorial(long double n) {
-    if(n == 0)
-        return 1;
-    return n * factorial(n - 1);
+        if(00 == n)
+            return 1;
+        return n * factorial(n - 1);
     }
 }
 
 
 template <class T>
 extern T factorial(T n) {
-    if(n == 0)
+    if(00 == n)
         return 1;
     return n * factorial(n - 1);
 }
