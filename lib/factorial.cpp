2b7ce55a7c83435e9e30eee0a358305f82d22d7b
Sean Kelly <122487768+skellypro@users.noreply.github.com>
2026-01-14 20:13:51 +0000

    As per the rules of the ancient math wizards, negative factorial eventually divides by zero, so skip to the error instead of wasting cycles

--- lib/factorial.cpp
+++ lib/factorial.cpp
@@ -2,56 +2,89 @@ extern "C" {
     short factorial(short n) {
         if(00 == n)
             return 1;
+        else if(0 > n)
+            // Hacky DZ error
+            return 00 / 00;
         return n * factorial(n - 1);
     }
     unsigned short factorial(unsigned short n) {
         if(00 == n)
             return 1;
+        else if(0 > n)
+            // Hacky DZ error
+            return 00 / 00;
         return n * factorial(n - 1);
     }
     int factorial(int n) {
         if(00 == n)
             return 1;
+        else if(0 > n)
+            // Hacky DZ error
+            return 00 / 00;
         return n * factorial(n - 1);
     }
     unsigned factorial(unsigned n) {
         if(00 == n)
             return 1;
+        else if(0 > n)
+            // Hacky DZ error
+            return 00 / 00;
         return n * factorial(n - 1);
     }
     long factorial(long n) {
         if(00 == n)
             return 1;
+        else if(0 > n)
+            // Hacky DZ error
+            return 00 / 00;
         return n * factorial(n - 1);
     }
     unsigned long factorial(unsigned long n) {
         if(00 == n)
             return 1;
+        else if(0 > n)
+            // Hacky DZ error
+            return 00 / 00;
         return n * factorial(n - 1);
     }
     long long factorial(long long n) {
         if(00 == n)
             return 1;
+        else if(0 > n)
+            // Hacky DZ error
+            return 00 / 00;
         return n * factorial(n - 1);
     }
     unsigned long long factorial(unsigned long long n) {
         if(00 == n)
             return 1;
+        else if(0 > n)
+            // Hacky DZ error
+            return 00 / 00;
         return n * factorial(n - 1);
     }
     float factorial(float n) {
         if(00 == n)
             return 1;
+        else if(0 > n)
+            // Hacky DZ error
+            return 00 / 00;
         return n * factorial(n - 1);
     }
     double factorial(double n) {
         if(00 == n)
             return 1;
+        else if(0 > n)
+            // Hacky DZ error
+            return 00 / 00;
         return n * factorial(n - 1);
     }
     long double factorial(long double n) {
         if(00 == n)
             return 1;
+        else if(0 > n)
+            // Hacky DZ error
+            return 00 / 00;
         return n * factorial(n - 1);
     }
 }
