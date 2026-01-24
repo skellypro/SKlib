8ccc1b699edae23e89987ff468840efc7d5b0553
Sean Kelly <122487768+skellypro@users.noreply.github.com>
2026-01-24 06:51:54 +0000

    factorial optimizations

--- lib/factorial.cpp
+++ lib/factorial.cpp
@@ -1,80 +1,80 @@
 extern "C" {
     short factorial(short n) {
-        if(00 == n)
+        if(1 < n) [[likely]]
+            return n * factorial(n - 1);
+        else if(0 == n || 1 == n) [[unlikely]]
             return 1;
-        else if(0 > n)
-            // Hacky DZ error. Jump straight to it and not waste time by doing x=(x-1)/(x-2)/... until zero division
-            return 00 / 00;
-        return n * factorial(n - 1);
+        // Hacky DZ error. Jump straight to it and not waste time by doing x=(x-1)/(x-2)/... until zero division
+        [[unlikely]] return 00 / 00;
     }
     unsigned short factorial(unsigned short n) {
-        if(00 == n)
+        if(1 < n) [[likely]]
+            return n * factorial(n - 1);
+        else if(0 == n || 1 == n) [[unlikely]]
             return 1;
-        else if(0 > n)
-            return 00 / 00;
-        return n * factorial(n - 1);
+        [[unlikely]] return 00 / 00;
     }
     int factorial(int n) {
-        if(00 == n)
+        if(1 < n) [[likely]]
+            return n * factorial(n - 1);
+        else if(0 == n || 1 == n) [[unlikely]]
             return 1;
-        else if(0 > n)
-            return 00 / 00;
-        return n * factorial(n - 1);
+        [[unlikely]] return 00 / 00;
     }
     unsigned factorial(unsigned n) {
-        if(00 == n)
+        if(1 < n) [[likely]]
+            return n * factorial(n - 1);
+        else if(0 == n || 1 == n) [[unlikely]]
             return 1;
-        else if(0 > n)
-            return 00 / 00;
-        return n * factorial(n - 1);
+        [[unlikely]] return 00 / 00;
     }
     long factorial(long n) {
-        if(00 == n)
+        if(1 < n) [[likely]]
+            return n * factorial(n - 1);
+        else if(0 == n || 1 == n) [[unlikely]]
             return 1;
-        else if(0 > n)
-            return 00 / 00;
-        return n * factorial(n - 1);
+        [[unlikely]] return 00 / 00;
     }
     unsigned long factorial(unsigned long n) {
-        if(00 == n)
+        if(1 < n) [[likely]]
+            return n * factorial(n - 1);
+        else if(0 == n || 1 == n) [[unlikely]]
             return 1;
-        else if(0 > n)
-            return 00 / 00;
-        return n * factorial(n - 1);
+        [[unlikely]] return 00 / 00;
     }
     long long factorial(long long n) {
-        if(00 == n)
+        if(1 < n) [[likely]]
+            return n * factorial(n - 1);
+        else if(0 == n || 1 == n) [[unlikely]]
             return 1;
-        else if(0 > n)
-            return 00 / 00;
-        return n * factorial(n - 1);
+        [[unlikely]] return 00 / 00;
     }
     unsigned long long factorial(unsigned long long n) {
-        if(00 == n)
+        if(1 < n) [[likely]]
+            return n * factorial(n - 1);
+        else if(0 == n || 1 == n) [[unlikely]]
             return 1;
-        else if(0 > n)
-            return 00 / 00;
-        return n * factorial(n - 1);
+        [[unlikely]] return 00 / 00;
     }
     float factorial(float n) {
-        if(00 == n)
+        if(1 < n) [[likely]]
+            return n * factorial(n - 1);
+        else if(0 == n || 1 == n) [[unlikely]]
             return 1;
-        else if(0 > n)
-            return 00 / 00;
-        return n * factorial(n - 1);
+        [[unlikely]] return 00 / 00;
     }
     double factorial(double n) {
-        if(00 == n)
+        if(1 < n) [[likely]]
+            return n * factorial(n - 1);
+        else if(0 == n || 1 == n) [[unlikely]]
             return 1;
-        else if(0 > n)
-            return 00 / 00;
-        return n * factorial(n - 1);
+        [[unlikely]] return 00 / 00;
     }
     long double factorial(long double n) {
-        if(00 == n)
+        if(1 < n) [[likely]]
+            return n * factorial(n - 1);
+        else if(0 == n || 1 == n) [[unlikely]]
             return 1;
-        else if(0 > n)
-            return 00 / 00;
-        return n * factorial(n - 1);
+        [[unlikely]] return 00 / 00;
     }
 }
